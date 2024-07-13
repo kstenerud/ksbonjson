22cec548e7d947a866d730a832624cb2fd5d8e0c
Karl Stenerud <kstenerud@gmail.com>
2024-07-13 08:15:51 +0200

    Clean up includes

--- executable/src/main.c
+++ executable/src/main.c
@@ -31,7 +31,6 @@
 #include <libgen.h>
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
--- library/src/KSBONJSONDecoder.c
+++ library/src/KSBONJSONDecoder.c
@@ -25,7 +25,6 @@
 //
 
 #include <ksbonjson/KSBONJSONDecoder.h>
-#include <stddef.h>
 
 
 // ============================================================================
